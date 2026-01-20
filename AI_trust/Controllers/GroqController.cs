7fc9ceba8113d07aa69a68f5e29d778193ae5d59
trandoanhkhoa <trandoanhkhoa192837@gmail.com>
2026-01-20 15:26:58 +0700

    Fix groq

--- AI_trust/Controllers/GroqController.cs
+++ AI_trust/Controllers/GroqController.cs
@@ -227,7 +227,7 @@ namespace AI_trust.Controllers
                 }
                 else
                 {
-                    if (request.questiontrytimes < 1)
+                    if (request.questiontrytimes <=1)
                     {
                         finalPrompt = $@"
                      {historyBlock}
